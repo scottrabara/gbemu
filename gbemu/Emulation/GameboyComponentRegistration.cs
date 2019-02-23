55c3e1038f075baba1976079f0837cb9bf6eb966
Scott Rabara <scottrabara@gmail.com>
2019-02-22 18:08:38 -0800

    Add rest of component types to registration

--- gbemu/Emulation/GameboyComponentRegistration.cs
+++ gbemu/Emulation/GameboyComponentRegistration.cs
@@ -11,16 +11,35 @@ namespace GBEmu.Emulation
 
             // Register the passed instance of gameboy
             builder.RegisterInstance(gameboy)
-                .OnActivated(e => e.Instance.CartridgeReader = e.Context.Resolve<CartridgeReader>());
+                .OnActivated(e =>
+                {
+                    e.Instance.CartridgeReader = e.Context.Resolve<CartridgeReader>();
+                    e.Instance.MemoryController = e.Context.Resolve<MemoryController>();
+                    e.Instance.Processor = e.Context.Resolve<Processor>();
+                });
 
             builder.RegisterType<CartridgeReader>()
                 .InstancePerLifetimeScope()
                 .OnActivated(e => e.Instance.Memory = e.Context.Resolve<Memory>());
 
+            builder.RegisterType<MemoryController>()
+                .InstancePerLifetimeScope()
+                .OnActivated(e => 
+                {
+                    e.Instance.Memory = e.Context.Resolve<Memory>();
+                    e.Instance.GraphicsProcessor = e.Context.Resolve<GraphicsProcessor>();
+                    e.Instance.Processor = e.Context.Resolve<Processor>();
+                });
+
+            builder.RegisterType<Processor>()
+                .InstancePerLifetimeScope()
+                .OnActivated(e => e.Instance.MemoryController = e.Context.Resolve<MemoryController>());
+
             builder.RegisterType<Memory>()
                 .InstancePerLifetimeScope();
 
-            // TODO: Register rest of types
+            builder.RegisterType<GraphicsProcessor>()
+                .InstancePerLifetimeScope();
 
             var container = builder.Build();
            
