4c65f85ea697920c6e4c60a8f47a47b5950008de
Scott Rabara <scottrabara@gmail.com>
2019-02-26 11:26:33 -0800

    Added Register and MemoryBased StringMaps fopr ParamOne

--- gbemu/Emulation/Processing/StringMaps/ParamOneMap.cs
+++ gbemu/Emulation/Processing/StringMaps/ParamOneMap.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace GBEmu.Emulation.Processing.StringMaps
 {
     /// <summary>
-    /// Map for representing all possibilities of an parameter given an opcode.
+    /// Map for representing all possibilities of the first parameter given an opcode.
     /// Using a Hexadecimal representation of the 255 instruction set for the Gameboy CPU.
     /// Example: First four characters of A is 0020, opcode given is 11.
     ///     "0020" (hex string) = "0000000000100000" (binary string)
@@ -18,21 +18,112 @@ namespace GBEmu.Emulation.Processing.StringMaps
     {
         // TODO: Create maps for all registers given an opcode.
 
+        #region Register Based Constants
         const string A =
             "002000200020002000000000000000FFFFFFFFFFFFFFFFFF090909090909A222";
+        const string B =
+            "0E00000000000000FF0000000000000000000000000000000000000000000000";
+        const string C =
+            "000E00000000000000FF00000000000000000000000000000000000000000000";
+        const string D =
+            "00000E00000000000000FF000000000000000000000000000000000000000000";
+        const string E =
+            "0000000E00000000000000FF0000000000000000000000000000000000000000";
+        const string H =
+            "000000000E00000000000000FF00000000000000000000000000000000000000";
+        const string L =
+            "00000000000E00000000000000FF000000000000000000000000000000000000";
         const string BC =
-            "7010701070107010000000000000000000000000000000000000000000004410";
+            "5010000000000000000000000000000000000000000000004410000000000000";
+        const string DE =
+            "0000501000000000000000000000000000000000000000000000441000000000";
+        const string HL =
+            "0000000050100000000000000000000000000000000000000000000044000080";
+        const string AF =
+            "0000000000000000000000000000000000000000000000000000000000004400";
+        const string SP =
+            "0000000000005010000000000000000000000000000000000000000000800040";
+        #endregion
+
+        #region Memory Based Constants
+        const string AddressAtBC =
+            "20002E00000000000000FF000000000000000000000000000000000000000000";
+        const string AddressAtDE =
+            "000020002E00000000000000FF00000000000000000000000000000000000000";
+        const string AddressAtHL = 
+            "0000000020002E00000000000000FF0000000000000000000000000000400000";
+        const string AddressAtC =
+            "0000000000000000000000000000000000000000000000000000000020000000";
+        #endregion
 
         public static IInstructionParam GetParamOne(Processor processor, int opcode)
         {
-            if (A.ToBinary()[opcode] == '1')
+            if (A.ContainsBitCharInMap(opcode))
             {
                 return new RegisterParam(processor.Registers.A);
             }
-            if (BC.ToBinary()[opcode] == '1')
+            if (B.ContainsBitCharInMap(opcode))
+            {
+                return new RegisterParam(processor.Registers.B);
+            }
+            if (C.ContainsBitCharInMap(opcode))
+            {
+                return new RegisterParam(processor.Registers.C);
+            }
+            if (D.ContainsBitCharInMap(opcode))
+            {
+                return new RegisterParam(processor.Registers.D);
+            }
+            if (E.ContainsBitCharInMap(opcode))
+            {
+                return new RegisterParam(processor.Registers.E);
+            }
+            if (H.ContainsBitCharInMap(opcode))
+            {
+                return new RegisterParam(processor.Registers.H);
+            }
+            if (L.ContainsBitCharInMap(opcode))
+            {
+                return new RegisterParam(processor.Registers.L);
+            }
+            if (BC.ContainsBitCharInMap(opcode))
             {
                 return new RegisterParam(processor.Registers.BC);
             }
+            if (DE.ContainsBitCharInMap(opcode))
+            {
+                return new RegisterParam(processor.Registers.DE);
+            }
+            if (HL.ContainsBitCharInMap(opcode))
+            {
+                return new RegisterParam(processor.Registers.HL);
+            }
+            if (AF.ContainsBitCharInMap(opcode))
+            {
+                return new RegisterParam(processor.Registers.AF);
+            }
+            if (SP.ContainsBitCharInMap(opcode))
+            {
+                return new RegisterParam(processor.Registers.SP);
+            }
+            if (AddressAtC.ContainsBitCharInMap(opcode))
+            {
+                return new MemoryParam(processor, processor.Registers.C.Value);
+            }
+            if (AddressAtBC.ContainsBitCharInMap(opcode))
+            {
+                return new MemoryParam(processor, processor.Registers.BC.Value);
+            }
+            if (AddressAtDE.ContainsBitCharInMap(opcode))
+            {
+                return new MemoryParam(processor, processor.Registers.DE.Value);
+            }
+            if (AddressAtHL.ContainsBitCharInMap(opcode))
+            {
+                return new MemoryParam(processor, processor.Registers.HL.Value);
+            }
+            // TODO: Create exception ParamNotFound
+            // For now return null
             return null;
         }
     }
