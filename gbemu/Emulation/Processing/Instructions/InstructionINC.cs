a033d3a759ecc03eba6f01a0fa206121bdabf257
Scott Rabara <scottrabara@gmail.com>
2019-03-01 10:42:20 -0800

    Added DEC, INC, SUB instructions; moved MemoryParam and RegisterParam into their own folder

---
+++ gbemu/Emulation/Processing/Instructions/InstructionINC.cs
@@ -0,0 +1,41 @@
+﻿using GBEmu.Emulation.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBEmu.Emulation.Processing.Instructions
+{
+    /// <summary>
+    /// Base instruction for all DEC instruction.
+    /// All DEC instructions have one operand.
+    /// </summary>
+    public class InstructionINC : IInstruction
+    {
+        public string BaseInstruction { get; set; } = "INC {0}";
+        public IInstructionParam[] Params { get; set; }
+        public IInstructionParam ParamOne => Params[0];
+        public string ParsedInstruction => GetParsedInstruction();
+        public int Ticks { get; set; }
+        public int Opcode { get; set; }
+
+        public Action Action => GetAction();
+
+        private string GetParsedInstruction()
+        {
+            return string.Format(
+                BaseInstruction,
+                ParamOne == null ? "null" : ParamOne.ToString());
+        }
+
+        private Action GetAction()
+        {
+            return () =>
+            {
+                if (ParamOne != null)
+                {
+                    ParamOne.Value = ParamOne.Value == 255 ? 255 : ParamOne.Value + 1;
+                }
+            };
+        }
+    }
+}
---
+++ gbemu/Emulation/Processing/Instructions/InstructionSUB.cs
@@ -0,0 +1,43 @@
+﻿using GBEmu.Emulation.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBEmu.Emulation.Processing.Instructions
+{
+    /// <summary>
+    /// Base instruction for all SUB instruction.
+    /// All DEC instructions have one operand.
+    /// </summary>
+    public class InstructionSUB : IInstruction
+    {
+        public string BaseInstruction { get; set; } = "SUB {0}, {1}";
+        public IInstructionParam[] Params { get; set; }
+        public IInstructionParam ParamOne => Params[0];
+        public IInstructionParam ParamTwo => Params[1];
+        public string ParsedInstruction => GetParsedInstruction();
+        public int Ticks { get; set; }
+        public int Opcode { get; set; }
+
+        public Action Action => GetAction();
+
+        private string GetParsedInstruction()
+        {
+            return string.Format(
+                BaseInstruction,
+                ParamOne == null ? "null" : ParamOne.ToString(),
+                ParamTwo == null ? "null" : ParamTwo.ToString());
+        }
+
+        private Action GetAction()
+        {
+            return () =>
+            {
+                if (ParamOne != null)
+                {
+                    ParamOne.Value = (ParamOne.Value - ParamTwo.Value) < 0 ? 0 : ParamOne.Value - ParamTwo.Value;
+                }
+            };
+        }
+    }
+}
