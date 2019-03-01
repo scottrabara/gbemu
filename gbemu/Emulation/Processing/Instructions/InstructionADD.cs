e3aef06a6c9efa1caa79309ff0ca4d709827416c
Scott Rabara <scottrabara@gmail.com>
2019-02-28 21:01:53 -0800

    Added ADD instruction

---
+++ gbemu/Emulation/Processing/Instructions/InstructionADD.cs
@@ -0,0 +1,44 @@
+﻿using GBEmu.Emulation.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBEmu.Emulation.Processing.Instructions
+{
+    /// <summary>
+    /// Base instruction for all ADD instructions.
+    /// All ADD instructions have two operands.
+    /// Most of the time it's A
+    /// </summary>
+    public class InstructionADD : IInstruction
+    {
+        public string BaseInstruction { get; set; } = "ADD {0}, {1}";
+        public IInstructionParam[] Params { get; set; }
+        public IInstructionParam ParamOne => Params[0];
+        public IInstructionParam ParamTwo => Params[1];
+        public string ParsedInstruction => GetParsedInstruction();
+        public int Ticks { get; set; }
+        public int Opcode { get; set; }
+
+        public Action Action => GetAction();
+
+        private string GetParsedInstruction()
+        {
+            return string.Format(
+                BaseInstruction,
+                ParamOne == null ? "null" : ParamOne.ToString(),
+                ParamTwo == null ? "null" : ParamTwo.ToString());
+        }
+
+        private Action GetAction()
+        {
+            return () =>
+            {
+                if (ParamOne != null && ParamTwo != null)
+                {
+                    ParamOne.Value += ParamTwo.Value;
+                }
+            };
+        }
+    }
+}
